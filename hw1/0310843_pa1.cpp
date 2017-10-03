80e14d43766fcd7ce4badaed60faa6e0b4cea561
HuaTsai <a0910666734@gmail.com>
2017-10-03 11:46:39 -0700

    change to google format

--- hw1/0310843_pa1.cpp
+++ hw1/0310843_pa1.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <deque>
+#include <fstream>
+#include <iostream>
 #include <queue>
 #include <stack>
+#include <string>
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   ifstream fin;
   ofstream fout;
   fin.open(argv[1]);
   fout.open(argv[2]);
-  if ( !fin ) {
+  if (!fin) {
     cerr << "File could not be opened" << endl;
     return 0;
   }
@@ -20,12 +20,11 @@ int main(int argc, char* argv[]) {
   deque<string> deque1;
   deque<string> deque2;
   string cur;
-  while ( !fin.eof() ) {
-    getline(fin,cur);
+  while (!fin.eof()) {
+    getline(fin, cur);
     cout << cur << endl;
   }
   fin.close();
   fout.close();
   return 0;
 }
-	
