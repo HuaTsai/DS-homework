c5fe503af31b75fd2a9627bc56612fb666580a30
HuaTsai <a0910666734@gmail.com>
2017-10-04 02:37:59 +0800

    hw1

---
+++ hw1/0310843_pa1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <deque>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  ifstream fin;
+  ofstream fout;
+  fin.open(argv[1]);
+  fout.open(argv[2]);
+  if ( !fin ) {
+    cerr << "File could not be opened" << endl;
+    return 0;
+  }
+  stack<deque<string>> mystack;
+  queue<deque<string>> myqueue;
+  deque<string> deque1;
+  deque<string> deque2;
+  string cur;
+  while ( !fin.eof() ) {
+    getline(fin,cur);
+    cout << cur << endl;
+  }
+  fin.close();
+  fout.close();
+  return 0;
+}
+	
